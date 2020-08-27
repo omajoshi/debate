a0cf38b8751c29dc1a5dab2269cb7fcf680226fc
Om Joshi <omajoshi9@gmail.com>
2020-08-27 05:10:00 +0000

    Created Manage Topics view

--- extemp/urls.py
+++ extemp/urls.py
@@ -3,9 +3,10 @@ from .views import *
 
 app_name = 'extemp'
 urlpatterns = [
-    path('create/', TournamentCreate.as_view(), name='tournament_create'),
-    path('<int:pk>/', TournamentDetail.as_view(), name='tournament_detail'),
-    path('round/<int:pk>/', manage_topics, name='manage_topics'),
+    path('tournament/create/', TournamentCreate.as_view(), name='tournament_create'),
+    path('tournament/<int:pk>/', TournamentDetail.as_view(), name='tournament_detail'),
+    path('round/<int:pk>/sections', manage_sections, name='manage_sections'),
+    path('round/<int:pk>/topics', manage_topics, name='manage_topics'),
     # path('round/<int:pk>/', SectionDetail.as_view(), name='section_detail2'),
     path('section/<int:pk>/', SectionDetail.as_view(), name='section_detail'),
     path('', TournamentList.as_view(), name='tournament_list'),
--- extemp/views.py
+++ extemp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
@@ -13,17 +13,24 @@ def create_view(request):
 def view_available_topics():
     pass
 
-
-def create_topic(request):
-    for section in topic.round.section_set.all():
-        TopicInstance.objects.create(topic=topic, section=section)
-
-
+def manage_sections(request, pk):
+    return
 
 
 def manage_topics(request, pk):
     round = get_object_or_404(Round, pk=pk)
-    return render(request, 'extemp/round_detail.html', context={'round': round})
+    if request.method == 'POST':
+        for x in range(1, 21):
+            if (c:=request.POST.get(f'code-{x}')) and (q:=request.POST.get(f'question-{x}')):
+                t, created = round.topic_set.update_or_create(code=c, defaults={'question': q})
+                if created:
+                    for section in round.section_set.all():
+                        section.topicinstance_set.create(topic=t)
+            elif (p:=request.POST.get(f'pk-{x}')):
+               Topic.objects.get(pk=p).delete()
+        return redirect(round.event.tournament)
+    topics = (* (round.topic_set.order_by('code')), *([] for x in range(20-round.topic_set.count())))
+    return render(request, 'extemp/manage_topics.html', context={'round': round, 'topics': topics})
 
 
 def draw(request):
