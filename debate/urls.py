1a5da027ddc2482c4b1fd9f4f9bdd2395f0fc19c
Om Joshi <om@omajoshi.com>
2020-10-17 09:07:37 -0500

    routing / to extemp

--- debate/urls.py
+++ debate/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('extemp/', include('extemp.urls')),
+    path('', include('extemp.urls')),
 ]
